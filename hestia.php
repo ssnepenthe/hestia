c6610ee2e70da7f2c7937774649323ba1bd04320
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-02-26 20:47:05 -0700

    Modify plugin bootstrap for new plugin class.

--- hestia.php
+++ hestia.php
@@ -19,26 +19,26 @@
  * Domain Path:
  */
 
-namespace SSNepenthe\Hestia;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-$plugin_root = dirname( __FILE__ );
+$plugin_root = plugin_dir_path( __FILE__ );
 
-require_once $plugin_root . '/src/shortcodes.php';
+if ( file_exists( $plugin_root . '/vendor/autoload.php' ) ) {
+	require_once $plugin_root . '/vendor/autoload.php';
+}
 
 /**
- * Delay plugin initialization until the 'init' hook.
+ * Initialize plugin on the 'init' hook.
  *
  * @return void
  */
-function init() {
-	add_shortcode( 'ancestors',   __NAMESPACE__ . '\\ancestors_handler' );
-	add_shortcode( 'attachments', __NAMESPACE__ . '\\attachments_handler' );
-	add_shortcode( 'children',    __NAMESPACE__ . '\\children_handler' );
-	// add_shortcode( 'family',      __NAMESPACE__ . '\\family_handler' );
-	add_shortcode( 'siblings',    __NAMESPACE__ . '\\siblings_handler' );
+function hestia_init() {
+	$name = 'hestia';
+	$version = '0.1.0';
+
+	$hestia = new \SSNepenthe\Hestia\Hestia( $name, $version );
+	$hestia->init();
 }
-add_action( 'init', __NAMESPACE__ . '\\init' );
+add_action( 'init', 'hestia_init' );
