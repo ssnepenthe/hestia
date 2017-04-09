80e615d3bcbd96c9b98a1508131350d7357256e9
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-04-09 14:45:53 -0700

    Slight ancestor test refactor/cleanup.

--- tests/test-ancestors.php
+++ tests/test-ancestors.php
@@ -1,32 +1,56 @@
 <?php
 
 class Ancestors_Test extends WP_UnitTestCase {
+	protected $hestia_attachments = [];
+	protected $hestia_posts = [];
+
+	function setUp() {
+		parent::setUp();
+
+		$this->hestia_posts['first'] = $this->factory()->post->create_and_get( [
+			'post_type' => 'page',
+		] );
+		$this->hestia_posts['second'] = $this->factory()->post->create_and_get( [
+			'post_parent' => $this->hestia_posts['first']->ID,
+			'post_type' => 'page',
+		] );
+		$this->hestia_posts['third'] = $this->factory()->post->create_and_get( [
+			'post_parent' => $this->hestia_posts['second']->ID,
+			'post_type' => 'page',
+		] );
+
+		$this->hestia_attachments['first'] = $this->factory()->attachment->create_object( [
+			'file' => 'image.jpg',
+			'post_parent' => $this->hestia_posts['second']->ID,
+			'post_mime_type' => 'image/jpeg',
+		] );
+	}
+
+	function tearDown() {
+		$this->hestia_attachments = [];
+		$this->hestia_posts = [];
+	}
+
 	/** @test */
 	function basic_output() {
-		$first = $this->factory()->post->create_and_get( [
-			'post_type' => 'page',
-		] );
-		$second = $this->factory()->post->create_and_get( [
-			'post_parent' => $first->ID,
-			'post_type' => 'page',
-		] );
-		$third = $this->factory()->post->create_and_get( [
-			'post_parent' => $second->ID,
-			'post_type' => 'page',
-		] );
-
 		add_filter( 'hestia_ancestors_cache_lifetime', '__return_zero' );
 
-		$GLOBALS['post'] = $third;
+		$GLOBALS['post'] = $this->hestia_posts['third'];
 
-		$rendered = sprintf( '<div class="hestia-ancestor hestia-wrap post-%1$s">
+		$rendered = sprintf(
+			'<div class="hestia-ancestor hestia-wrap post-%1$s">
 		<a href="http://example.org/?page_id=%1$s">
 						%2$s		</a>
 	</div>
 	<div class="hestia-ancestor hestia-wrap post-%3$s">
 		<a href="http://example.org/?page_id=%3$s">
 						%4$s		</a>
-	</div>', $first->ID, $first->post_title, $second->ID, $second->post_title );
+	</div>',
+			$this->hestia_posts['first']->ID,
+			$this->hestia_posts['first']->post_title,
+			$this->hestia_posts['second']->ID,
+			$this->hestia_posts['second']->post_title
+		);
 
 		$this->assertEquals( $rendered, trim( do_shortcode( '[ancestors]' ) ) );
 
@@ -35,30 +59,24 @@ class Ancestors_Test extends WP_UnitTestCase {
 
 	/** @test */
 	function descending_order() {
-		$first = $this->factory()->post->create_and_get( [
-			'post_type' => 'page',
-		] );
-		$second = $this->factory()->post->create_and_get( [
-			'post_parent' => $first->ID,
-			'post_type' => 'page',
-		] );
-		$third = $this->factory()->post->create_and_get( [
-			'post_parent' => $second->ID,
-			'post_type' => 'page',
-		] );
-
 		add_filter( 'hestia_ancestors_cache_lifetime', '__return_zero' );
 
-		$GLOBALS['post'] = $third;
+		$GLOBALS['post'] = $this->hestia_posts['third'];
 
-		$rendered = sprintf( '<div class="hestia-ancestor hestia-wrap post-%1$s">
+		$rendered = sprintf(
+			'<div class="hestia-ancestor hestia-wrap post-%1$s">
 		<a href="http://example.org/?page_id=%1$s">
 						%2$s		</a>
 	</div>
 	<div class="hestia-ancestor hestia-wrap post-%3$s">
 		<a href="http://example.org/?page_id=%3$s">
 						%4$s		</a>
-	</div>', $second->ID, $second->post_title, $first->ID, $first->post_title );
+	</div>',
+			$this->hestia_posts['second']->ID,
+			$this->hestia_posts['second']->post_title,
+			$this->hestia_posts['first']->ID,
+			$this->hestia_posts['first']->post_title
+		);
 
 		$this->assertEquals(
 			$rendered,
@@ -70,32 +88,15 @@ class Ancestors_Test extends WP_UnitTestCase {
 
 	/** @test */
 	function with_thumbnails() {
-		$first = $this->factory()->post->create_and_get( [
-			'post_type' => 'page',
-		] );
-		$second = $this->factory()->post->create_and_get( [
-			'post_parent' => $first->ID,
-			'post_type' => 'page',
-		] );
-		$third = $this->factory()->post->create_and_get( [
-			'post_parent' => $second->ID,
-			'post_type' => 'page',
-		] );
-
-		$attachment_id = $this->factory()->attachment->create_object( [
-			'file' => 'image.jpg',
-			'post_parent' => $second->ID,
-			'post_type' => 'attachment',
-			'post_mime_type' => 'image/jpeg',
-		] );
-
-		set_post_thumbnail( $second, $attachment_id );
-
 		add_filter( 'hestia_ancestors_cache_lifetime', '__return_zero' );
 
-		$GLOBALS['post'] = $third;
+		set_post_thumbnail(
+			$this->hestia_posts['second'],
+			$this->hestia_attachments['first']
+		);
 
-		$image_string = get_the_post_thumbnail( $second->ID );
+		$GLOBALS['post'] = $this->hestia_posts['third'];
+
 		$rendered = sprintf(
 			'<div class="hestia-ancestor hestia-wrap post-%1$s">
 		<a href="http://example.org/?page_id=%1$s">
@@ -105,11 +106,11 @@ class Ancestors_Test extends WP_UnitTestCase {
 		<a href="http://example.org/?page_id=%3$s">
 			%4$s			%5$s		</a>
 	</div>',
-			$first->ID,
-			$first->post_title,
-			$second->ID,
-			$image_string,
-			$second->post_title
+			$this->hestia_posts['first']->ID,
+			$this->hestia_posts['first']->post_title,
+			$this->hestia_posts['second']->ID,
+			get_the_post_thumbnail( $this->hestia_posts['second']->ID ),
+			$this->hestia_posts['second']->post_title
 		);
 
 		$this->assertEquals(
@@ -122,32 +123,15 @@ class Ancestors_Test extends WP_UnitTestCase {
 
 	/** @test */
 	function descending_with_thumbnails() {
-		$first = $this->factory()->post->create_and_get( [
-			'post_type' => 'page',
-		] );
-		$second = $this->factory()->post->create_and_get( [
-			'post_parent' => $first->ID,
-			'post_type' => 'page',
-		] );
-		$third = $this->factory()->post->create_and_get( [
-			'post_parent' => $second->ID,
-			'post_type' => 'page',
-		] );
-
-		$attachment_id = $this->factory()->attachment->create_object( [
-			'file' => 'image.jpg',
-			'post_parent' => $second->ID,
-			'post_type' => 'attachment',
-			'post_mime_type' => 'image/jpeg',
-		] );
-
-		set_post_thumbnail( $second, $attachment_id );
-
 		add_filter( 'hestia_ancestors_cache_lifetime', '__return_zero' );
 
-		$GLOBALS['post'] = $third;
+		set_post_thumbnail(
+			$this->hestia_posts['second'],
+			$this->hestia_attachments['first']
+		);
 
-		$image_string = get_the_post_thumbnail( $second->ID );
+		$GLOBALS['post'] = $this->hestia_posts['third'];
+
 		$rendered = sprintf(
 			'<div class="has-post-thumbnail hestia-ancestor hestia-wrap post-%1$s">
 		<a href="http://example.org/?page_id=%1$s">
@@ -157,11 +141,11 @@ class Ancestors_Test extends WP_UnitTestCase {
 		<a href="http://example.org/?page_id=%4$s">
 						%5$s		</a>
 	</div>',
-			$second->ID,
-			$image_string,
-			$second->post_title,
-			$first->ID,
-			$first->post_title
+			$this->hestia_posts['second']->ID,
+			get_the_post_thumbnail( $this->hestia_posts['second']->ID ),
+			$this->hestia_posts['second']->post_title,
+			$this->hestia_posts['first']->ID,
+			$this->hestia_posts['first']->post_title
 		);
 
 		$this->assertEquals(
