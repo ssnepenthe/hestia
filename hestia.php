4958d55c1d0e40fe2f30024c3cdfeaf64194586f
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-27 15:28:10 -0700

    Move plugin init to dedicated function

--- hestia.php
+++ hestia.php
@@ -22,6 +22,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Handles plugin initialization.
+ */
+function _hestia_init() {
+	$checker = WP_Requirements\Plugin_Checker::make( 'Hestia', __FILE__ )
+		// For transient key length.
+		->wp_at_least( '4.4' )
+		// Depends on ssnepenthe/metis.
+		->php_at_least( '7.0' );
+
+	if ( ! $checker->requirements_met() ) {
+		return $checker->deactivate_and_notify();
+	}
+
+	_hestia_instance()->init();
+}
+
+/**
+ * Static plugin instance getter.
+ *
+ * @return Metis\Package
+ */
+function _hestia_instance() {
+	static $instance = null;
+
+	if ( is_null( $instance ) ) {
+		$instance = new Metis\Package( [
+			Metis\Cache\Cache_Provider::class,
+			Metis\View\View_Provider::class,
+			SSNepenthe\Hestia\Hestia_Provider::class,
+			SSNepenthe\Hestia\Shortcode\Shortcode_Provider::class,
+			SSNepenthe\Hestia\Task\Task_Provider::class,
+		] );
+	}
+
+	return $instance;
+}
+
+/**
  * Include a file if it exists via "require_once".
  *
  * @param  string $file Path to file for inclusion.
@@ -33,24 +71,4 @@ function _hestia_require_if_exists( $file ) {
 }
 
 _hestia_require_if_exists( __DIR__ . '/vendor/autoload.php' );
-
-$hestia_checker = WP_Requirements\Plugin_Checker::make( 'Hestia', __FILE__ )
-	// For transient key length.
-	->wp_at_least( '4.4' )
-	// Depends on ssnepenthe/metis.
-	->php_at_least( '7.0' );
-
-if ( $hestia_checker->requirements_met() ) {
-	$hestia_plugin = new Metis\Package( [
-		Metis\Cache\Cache_Provider::class,
-		Metis\View\View_Provider::class,
-		SSNepenthe\Hestia\Hestia_Provider::class,
-		SSNepenthe\Hestia\Shortcode\Shortcode_Provider::class,
-		SSNepenthe\Hestia\Task\Task_Provider::class,
-	] );
-	$hestia_plugin->init();
-} else {
-	$hestia_checker->deactivate_and_notify();
-}
-
-unset( $hestia_checker, $hestia_plugin );
+_hestia_init();
