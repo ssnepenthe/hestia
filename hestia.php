15d051c4972ffe96f4ae58eac2fee7b295c8bb5f
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-08-20 13:16:07 -0700

    Revert "Reduce actual required PHP version to 5.4"
    This reverts commit 6005aa8a1f1254e8234d4ff99ee280058b6fcf1b.

--- hestia.php
+++ hestia.php
@@ -32,8 +32,8 @@ function _hestia_init() {
 	}
 
 	$checker = WP_Requirements\Plugin_Checker::make( 'Hestia', __FILE__ )
-		// Traits and short array syntax.
-		->php_at_least( '5.4' );
+		// Function imports.
+		->php_at_least( '5.6' );
 
 	if ( ! $checker->requirements_met() ) {
 		$checker->deactivate_and_notify();
